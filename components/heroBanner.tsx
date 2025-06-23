16f0d6e8d2662b8a33b9ceef009aab58b13ebddb
Muskan Punshi <muskan.punshi@ideabox.pk>
2025-06-23 14:48:29 +0500

    changes

--- components/heroBanner.tsx
+++ components/heroBanner.tsx
@@ -24,13 +24,13 @@ const HeroBanner = () => {
             </Heading>
           </div>
           <div className="w-full lg:w-[40%] mt-10 lg:mt-0 fade-in-right  h-full">
-            <Paragraph className=" sm:text-[18px] md:text-[24px]   text-text-primary font-normal mb-10 lg:mb-5 leading-[1.6]">
+            <Paragraph className=" sm:text-[18px] md:text-[24px] lg:text-[28px]  text-text-primary font-normal mb-10 lg:mb-5 leading-[1.6]">
               We connect importers, exporters, local manufacturers, and
               customers, providing a seamless and reliable sourcing experience
               on this platform.
             </Paragraph>
             <Button
-              className="hover:bg-primary mt-8 max-sm:mt-0 bg-black text-white"
+              className="hover:bg-primary mt-8 px-8 py-4 max-sm:mt-0 bg-black text-white"
               href="#"
             >
               {" "}
