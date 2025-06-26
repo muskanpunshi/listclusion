01649e59649e41d25bc6993d4cc8920e33b61be5
Muhammad Adnan <58784149+Muhammad-Adnan71@users.noreply.github.com>
2025-06-26 17:27:38 +0500

    changes

---
+++ components/heading.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@utils/index";
+import React, { ReactNode } from "react";
+
+export default function Heading({
+  children,
+  className,
+  isHeadingH1 = false,
+}: {
+  isHeadingH1?: boolean;
+  children: ReactNode;
+  className?: string;
+}) {
+  return isHeadingH1 ? (
+    <h1
+      className={`font-primary text-[70px] max-lg:text-[40px] max-sm:text-[36px] leading-[1.25] capitalize text-secondary font-medium ${className}`}
+    >
+      {children}
+    </h1>
+  ) : (
+    <h2
+      className={cn([
+        "font-primary  text-secondary text-[64px] max-xl:text-[48px]  max-lg:text-[36px] max-sm:[24px] font-medium ",
+        className,
+      ])}
+    >
+      {children}
+    </h2>
+  );
+}
