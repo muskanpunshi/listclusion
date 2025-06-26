920328d2c9978f8fe991c4b0a2d2825345b9ac54
Muhammad Adnan <58784149+Muhammad-Adnan71@users.noreply.github.com>
2025-06-26 21:34:14 +0500

    changes

---
+++ components/Button.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import { cn } from "@utils/index";
+
+type LinkButtonProps = {
+  href: string;
+  iconOnly?: boolean;
+  className?: string;
+  children?: ReactNode;
+};
+
+const Button: React.FC<LinkButtonProps> = ({
+  href,
+
+  iconOnly = false,
+
+  className = "",
+  children,
+}) => {
+  if (iconOnly) {
+    return (
+      <Link
+        className={cn(
+          "w-full rounded-full bg-black hover:bg-primary text-white flex items-center justify-center transition-all duration-300 ease-in-out ",
+          className
+        )}
+        href={href}
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <Link
+      className={cn(
+        "w-fit px-8 py-4  bg-black hover:bg-primary text-white flex items-center justify-center transition-all duration-300 ease-in-out ",
+        className
+      )}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default Button;
